4b9babd85e4e460afc815bda00032db56f4a106f
Roberto Sequeira <robertosequeira@users.noreply.github.com>
2016-03-18 08:58:01 -0600

    Create blocks_procs_lambdas.rb

---
+++ blocks_procs_lambdas.rb
@@ -0,0 +1,11 @@
+# Reference
+# https://rubymonk.com/learning/books/4-ruby-primer-ascent/chapters/18-blocks/lessons/64-blocks-procs-lambdas
+# http://awaxman11.github.io/blog/2013/08/05/what-is-the-difference-between-a-block/
+
+#Block
+
+# Proc
+http://ruby-doc.org/core-2.2.3/Kernel.html#method-i-proc
+
+# Lambda
+http://ruby-doc.org/core-2.2.3/Kernel.html#method-i-lambda
